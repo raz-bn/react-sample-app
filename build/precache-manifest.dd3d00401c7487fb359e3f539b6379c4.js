5788d37cc56aa20f14cc4266e78a2fe4903ae0bc
Raz Ben Netanel <52315689+raz-bn@users.noreply.github.com>
2022-01-11 14:28:20 +0200

    init

---
+++ build/precache-manifest.dd3d00401c7487fb359e3f539b6379c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfa58cc55a085bc0ba3a",
+    "url": "./static/css/main.e7434f04.chunk.css"
+  },
+  {
+    "revision": "bfa58cc55a085bc0ba3a",
+    "url": "./static/js/main.bfa58cc5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "b833d4679e575f81df15",
+    "url": "./static/js/2.b833d467.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5dbcefac95f05ec2d4c9943c3177168d",
+    "url": "./index.html"
+  }
+];
